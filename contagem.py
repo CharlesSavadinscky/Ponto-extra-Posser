c7f63e5b641901e1cace52d3b8244af4c3a6c444
Charles Savadinscky <131482658+CharlesSavadinscky@users.noreply.github.com>
2024-08-29 14:56:20 -0300

    Add files via upload

---
+++ contagem.py
@@ -0,0 +1,25 @@
+import threading
+import time
+
+#Thread 1
+def thread1():
+    for i in range(1,6):
+        print(f"Thread 1: {i}")
+        time.sleep(1)
+
+#Thread 2
+def thread2():
+    for i in range(1,6):
+        print(f"Thread 2: {i}")
+        time.sleep(1.5)
+
+thread1 = threading.Thread(target=thread1)
+thread2 = threading.Thread(target=thread2)
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+print("Cabo (;-;)")
